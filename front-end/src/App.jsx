301cea44164fdbf5378d4af86f1ab72e4a20a238
ghribik <g.hribik@gmail.com>
2023-01-03 14:58:32 -0800

    initial baseline for the project

---
+++ front-end/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+     <h1>Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
